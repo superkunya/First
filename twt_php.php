11901e14bf61c8f7682294fddcc78db2acb65e09
superkunya <superkunya@yahoo.com>
2015-07-21 23:40:38 -0400

    twt rus add

--- twt_php.php
+++ twt_php.php
@@ -12,11 +12,142 @@ $content = $connection->get('account/verify_credentials');
 $connection->post('statuses/update', array('status' => 'twit with http://designspicy.com - say hello')); ?>
 
 
+
+
+
+
+class TwitterOAuth extends Config
+{
+   const API_VERSION = '1.1';
+   const API_HOST = 'https://api.twitter.com';
+   const UPLOAD_HOST = 'https://upload.twitter.com';
+
+
+   /** @var Response details about the result of the last request */
+   private $response;
+   /** @var string|null Application bearer token */
+   private $bearer;
+   /** @var Consumer Twitter application details */
+   private $consumer;
+   /** @var Token|null User access token details */
+   private $token;
+   /** @var HmacSha1 OAuth 1 signature type used by Twitter */
+   private $signatureMethod;
+
+
+   /**
+    * Constructor
+    *
+    * @param string      $consumerKey      The Application Consumer Key
+    * @param string      $consumerSecret   The Application Consumer Secret
+    * @param string|null $oauthToken       The Client Token (optional)
+    * @param string|null $oauthTokenSecret The Client Token Secret (optional)
+    */
+   public function __construct($consumerKey, $consumerSecret, $oauthToken = null, $oauthTokenSecret = null)
+   {
+       $this->resetLastResponse();
+       $this->signatureMethod = new HmacSha1();
+       $this->consumer = new Consumer($consumerKey, $consumerSecret);
+       if (!empty($oauthToken) && !empty($oauthTokenSecret)) {
+           $this->token = new Token($oauthToken, $oauthTokenSecret);
+       }
+       if (empty($oauthToken) && !empty($oauthTokenSecret)) {
+           $this->bearer = $oauthTokenSecret;
+       }
+   }
+
+
+   /**
+    * @param string $oauthToken
+    * @param string $oauthTokenSecret
+    */
+   public function setOauthToken($oauthToken, $oauthTokenSecret)
+   {
+       $this->token = new Token($oauthToken, $oauthTokenSecret);
+   }
+
+
+   /**
+    * @return string|null
+    */
+   public function getLastApiPath()
+   {
+       return $this->response->getApiPath();
+   }
+
+
+   /**
+    * @return int
+    */
+   public function getLastHttpCode()
+   {
+       return $this->response->getHttpCode();
+   }
+
+
+   /**
+    * @return array
+    */
+   public function getLastXHeaders()
+   {
+       return $this->response->getXHeaders();
+   }
+
+
+   /**
+    * @return array|object|null
+    */
+   public function getLastBody()
+   {
+       return $this->response->getBody();
+   }
+
+
+   /**
+    * Resets the last response cache.
+    */
+   public function resetLastResponse()
+   {
+       $this->response = new Response();
+   }
+
+
+
+
+
+   /**
+    * Make POST requests to the API.
+    *
+    * @param string $path
+    * @param array  $parameters
+    *
+    * @return array|object
+    */
+   public function post($path, array $parameters = array())
+   {
+       return $this->http('POST', self::API_HOST, $path, $parameters);
+   }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 $parameters = $_POST;
 $message = $parameters['message'];
 $name =  $parameters['member'];
 $accounts = array (
-"lesya" => 'twitterIDLesya',
+"lesya" => 'lana-roxolana',
 'john' => 'twitterIDJohn'
 );
 $data = array(
